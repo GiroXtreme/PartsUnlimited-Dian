372a8c4ae155384f4f00f8881567f18c6f2fa07c
Eddy Gio Rodriguez <girox06@gmail.com>
2024-05-17 11:44:00 -0500

    Reversión de soft

--- src/PartsUnlimitedWebsite/Areas/Admin/Controllers/AdminController.cs
+++ src/PartsUnlimitedWebsite/Areas/Admin/Controllers/AdminController.cs
@@ -8,6 +8,8 @@ namespace PartsUnlimited.Areas.Admin.Controllers
 {
     //comentario remoto
     //Comentario local
+    //Cambio 1
+    //Cambio 2
     [Area(AdminConstants.Area)]
     [Authorize(AdminConstants.Role)]
     public abstract class AdminController : Controller
